67f9d717da599bfb7ceca9ec110fe559751aa441
Jules-Elvin Andrade <juleselvandrade@gmail.com>
2025-07-24 15:36:06 -0700

    Initial commit

---
+++ src/routes/NotFound.jsx
@@ -0,0 +1,12 @@
+
+const NotFound = () => {
+
+    return (
+        <>
+        <h1> <center> NOTHING HERE :( </center> </h1>
+        <h3> <center> (or maybe there is supposed to be... is there?) </center> </h3>
+        </>
+    )
+}
+
+export default NotFound
